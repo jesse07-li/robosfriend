3bac768c112b457f994083468fb8f2e587c0ce61
jesse-07-li <jessicalongane@Jessicas-MacBook-Air.local>
2024-12-27 08:22:49 +0100

    new files RobosFriend

---
+++ myapp/src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = ({id, name, username, email})=>{
+	return(
+		<div className='tc dib bg-light-green br3 pa2 ma2 grow bw3 shadow-5'>
+		<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+		<h2>{name}</h2>
+		<p>{email}</p>
+		</div>
+	)
+}
+
+
+
+
+
+
+
+
+
+
+export default Card;
+
